836b20b4756dec38cb6c6f12a05b6dbb606ee943
psranke <psranke@gmail.com>
2022-09-03 23:43:53 +0200

    Update rsdg.R
    update 1

--- R/rsdg.R
+++ R/rsdg.R
@@ -1,125 +1,135 @@
-## Clean Memory
-rm(list=ls())
-
-# load packages
-library(utils)
-library(stats)
-library(dplyr)
-library(reshape2)
-library(tidyr)
-library(tools)
-
-
-# read from zip-folder
-#data <- read.table(unz("/home/peterxps/Downloads", "Sales.dat"), nrows=10, header=T, quote="\"", sep=",")
-
-# load functions
-# myfun <- function(x){x + 7}
-# myfun(2)
-
-#zipdf <- unzip("/home/peterxps/Downloads/dwca-great_reed_warbler-v1.4.zip", list = TRUE)
-
-# the following line assuming the archive has only a single file
-
-
-#x <- unzip("/home/peterxps/Downloads/dwca-great_reed_warbler-v1.4.zip")
-
-
-
-
-## Set path for your working location
-#setwd("D:/blah")
-
-## unzipped it the file
-#y <- unzip("/home/peterxps/Downloads/dwca-great_reed_warbler-v1.4.zip")
-
-## Check file in the zipped file
-#list.files("/home/peterxps/Downloads/dwca-great_reed_warbler-v1.4.zip")
-
-
-
-
-tmpd <- '/home/peterxps/Dropbox/papers/Ranke_msc_GRW_habitat_preference'
-filename <-'dwca-great_reed_warbler-v1.5.zip'
-paste0(tmpd,'/',filename)
-
-tmpfolder <- tempfile()
-
-## extract just the child
-unzip(paste0(tmpd,'/',filename), exdir=tmpfolder)
-ff <- file.path(paste0(tmpd,'/',filename))
-index <- unzip(ff,list=TRUE)
-## Read the file
-temp = list.files(path = tmpfolder, pattern="*.txt")
-temp
-#unlink(ff) # deleting???
-
-# files is deleted...
-
-# temp files   CRAN requirement
-#file <- tempfile()
-
-
-#need for loop here
-#for(i in 1:length(temp)){
-event <- read.table(paste0(tmpd,'/',file_path_sans_ext(filename), '/',temp[1]), skip = 0, header=T, sep="\t")
-extendedmeasurementorfact <- read.table(paste0(tmpd,'/',file_path_sans_ext(filename), '/',temp[2]), skip = 0, header=T, sep="\t")
-materialsample <- read.table(paste0(tmpd,'/',file_path_sans_ext(filename), '/',temp[3]), skip = 0, header=T, sep="\t")
-occurrence <- read.table(paste0(tmpd,'/',file_path_sans_ext(filename), '/',temp[4]), skip = 0, header=T, sep="\t")
-resourcerelationship <- read.table(paste0(tmpd,'/',file_path_sans_ext(filename), '/',temp[5]), skip = 0, header=T, sep="\t")
-#}
-
-# find events with occurrences
-eventswithoccurr <- subset(event, (id %in% occurrence$eventID))
-
-# find events without occurrences
-eventswithoutoccurr <- subset(event, !(id %in% occurrence$eventID))
-
-# join in sample protocol information to event measurements or facts table
-extendedmeasurementorfact2 <-  merge(x = extendedmeasurementorfact, y = event[,c("id","samplingProtocol")], by.x = "id", by.y= "id")
-
-# join in sample protocol information to occurrence table
-occurrence2 <-  merge(x = occurrence, y = event[,c("id","samplingProtocol")], by.x = "id", by.y= "id")
-
-
-# first check sampling protocols, basis for iteration
-df <- data.frame(samplingsprotocols = levels(event$samplingProtocol))
-
-for(i in seq_along(levels(df$samplingsprotocols))){
-  # split events and mof by sampling protocols
-  assign(paste0("event_", df$samplingsprotocols[i]), data.frame(subset(event, samplingProtocol == df$samplingsprotocols[i])))
-  assign(paste0("mof_", df$samplingsprotocols[i]), data.frame(subset(extendedmeasurementorfact2, samplingProtocol == df$samplingsprotocols[i])))
-
-
-  # finding occurrences based on sample protocols
-  tab <- paste0("event_", df$samplingsprotocols[i])
-  assign(paste0("occurr_", df$samplingsprotocols[i]), data.frame(subset(occurrence2, (id %in% occurrence2$eventID) & (occurrence2$samplingProtocol.y %in% df$samplingsprotocols[i]))))
-
+enterFileName <- function(){
+  options(myproject.filename = readline(prompt="Enter file name: "))
 }
 
+rsdg <- function(filename){
 
-# dcast for mof on occurrence
+  ## Clean Memory
+  rm(list=ls())
 
-mof_capture_recapture_adults_wide <- dcast(mof_capture_recapture_adults,occurrenceID~measurementType,value.var = "measurementValue")
-write.table(mof_capture_recapture_adults_wide,"mof_capture_recapture_adults_wide.txt", quote = F, sep = ";",row.names = F, col.names = T)
+  # load packages
+  library(utils)
+  library(stats)
+  library(dplyr)
+  library(reshape2)
+  library(tidyr)
+  library(tools)
 
-mof_capture_recapture_nestlings_wide <- dcast(mof_capture_recapture_nestlings,occurrenceID~measurementType,value.var = "measurementValue")
-write.table(mof_capture_recapture_nestlings_wide,"mof_capture_recapture_nestlings_wide.txt", quote = F, sep = ";",row.names = F, col.names = T)
+  tmpd <- getwd()
+  #filename <-'dwca-great_reed_warbler-v1.5.zip'
+  filename <- getOption("myproject.filename")
+  paste0(tmpd,'/',filename)
+
+  tmpfolder <- tempfile()
+
+  ## extract just the child
+  unzip(paste0(tmpd,'/',filename), exdir=tmpfolder)
+  ff <- file.path(paste0(tmpd,'/',filename))
+  index <- unzip(ff,list=TRUE)
+  ## Read the file
+  temp = list.files(path = tmpfolder, pattern="*.txt")
+  temp
 
 
 
-# dcast for mof on event
+  for(i in 1:length(temp)){
+    assign(paste0("raw_", sub("\\..*", "", temp[i]) ), read.csv(paste0(tmpd,'/',file_path_sans_ext(filename), '/',temp[i]), skip = 0, header=T, sep="\t", stringsAsFactors=F), envir = .GlobalEnv)
+  }
 
-# issues with special characters in samplingProtocol
-mof_habitat_measurement <- `mof_habitat-measurement`
-mof_habitat_measurement_wide <- dcast(mof_habitat_measurement,id~measurementType,value.var = "measurementValue")
-write.table(mof_habitat_measurement_wide,"mof_habitat_measurement_wide.txt", quote = F, sep = ";",row.names = F, col.names = T)
-
-# issues with special characters in samplingProtocol
-mof_tree_measurement <- `mof_tree-measurement`
-mof_tree_measurement_wide <- dcast(mof_tree_measurement,id~measurementType,value.var = "measurementValue")
-write.table(mof_tree_measurement_wide,"mof_tree_measurement_wide.txt", quote = F, sep = ";",row.names = F, col.names = T)
+  # first check sampling protocols for special characters
+  raw_event$samplingProtocol <- gsub("[[:punct:]]", "_", raw_event$samplingProtocol)
 
 
+  # find events with occurrences
+  eventswithoccurr <- subset(raw_event, (id %in% raw_occurrence$eventID))
 
+  # find events without occurrences
+  eventswithoutoccurr <- subset(raw_event, !(id %in% raw_occurrence$eventID))
+
+
+
+  # join in sample protocol information to event measurements or facts table
+  extendedmeasurementorfact2 <-  merge(x = raw_extendedmeasurementorfact, y = raw_event[,c("id","samplingProtocol")], by.x = "id", by.y= "id")
+
+  # join in sample protocol information to occurrence table
+  occurrence2 <-  merge(x = raw_occurrence, y = raw_event[,c("id","samplingProtocol")], by.x = "id", by.y= "id")
+
+
+
+  # then check sampling protocols, basis for iteration
+  raw_event$samplingProtocol <- as.factor(as.character(raw_event$samplingProtocol))
+  dfsamplingsprotocols <- data.frame(samplingsprotocols = levels(raw_event$samplingProtocol), stringsAsFactors=T)
+
+
+  # for(i in seq_along(levels(dfsamplingsprotocols$samplingsprotocols))){
+  #   # split events and mof by sampling protocols
+  #   assign(paste0("event_", dfsamplingsprotocols$samplingsprotocols[i]), data.frame(subset(raw_event, samplingProtocol == dfsamplingsprotocols$samplingsprotocols[i])))
+  #   assign(paste0("mof_", dfsamplingsprotocols$samplingsprotocols[i]), data.frame(subset(extendedmeasurementorfact2, samplingProtocol == dfsamplingsprotocols$samplingsprotocols[i])))
+  #
+  #
+  #   # finding occurrences based on sample protocols
+  #   assign(paste0("occurr_", dfsamplingsprotocols$samplingsprotocols[i]), data.frame(subset(occurrence2, (id %in% occurrence2$eventID) & (occurrence2$samplingProtocol.y %in% dfsamplingsprotocols$samplingsprotocols[i]))))
+  #
+  # }
+
+
+  # OUTPUT TABLES
+
+
+  # dcast for mof on occurrence
+  # then check sampling protocols, basis for iteration
+  eventswithoccurr$samplingProtocol <- as.factor(as.character(eventswithoccurr$samplingProtocol))
+  dfoccurrsamplingsprotocols <- data.frame(samplingsprotocols = levels(eventswithoccurr$samplingProtocol), stringsAsFactors=T)
+  for(i in seq_along(levels(dfoccurrsamplingsprotocols$samplingsprotocols))){
+    #
+    assign(paste0("df_", dfoccurrsamplingsprotocols$samplingsprotocols[i]), dcast(subset(extendedmeasurementorfact2, samplingProtocol == dfoccurrsamplingsprotocols$samplingsprotocols[i]),occurrenceID~measurementType,value.var = "measurementValue"), envir = .GlobalEnv )
+    # variable description
+    assign(paste0("df_", dfoccurrsamplingsprotocols$samplingsprotocols[i],"_desc"), data.frame(subset(extendedmeasurementorfact2, samplingProtocol == dfoccurrsamplingsprotocols$samplingsprotocols[i]) %>% select(measurementType,measurementAccuracy,measurementUnit,measurementMethod) %>% unique()), envir = .GlobalEnv )
+  }
+
+
+  # dcast for mof on event
+  # then check sampling protocols, basis for iteration
+  eventswithoutoccurr$samplingProtocol <- as.factor(as.character(eventswithoutoccurr$samplingProtocol))
+  dfeventsamplingsprotocols <- data.frame(samplingsprotocols = levels(eventswithoutoccurr$samplingProtocol), stringsAsFactors=T)
+  for(i in seq_along(levels(dfeventsamplingsprotocols$samplingsprotocols))){
+    #
+    assign(paste0("df_", dfeventsamplingsprotocols$samplingsprotocols[i]), dcast(subset(extendedmeasurementorfact2, samplingProtocol == dfeventsamplingsprotocols$samplingsprotocols[i]),id~measurementType,value.var = "measurementValue"), envir = .GlobalEnv )
+    # variable description
+    assign(paste0("df_", dfeventsamplingsprotocols$samplingsprotocols[i],"_desc"), data.frame(subset(extendedmeasurementorfact2, samplingProtocol == dfeventsamplingsprotocols$samplingsprotocols[i]) %>% select(measurementType,measurementAccuracy,measurementUnit,measurementMethod) %>% unique()), envir = .GlobalEnv )
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  # remove subsets
+  # for(i in seq_along(levels(dfsamplingsprotocols$samplingsprotocols))){
+  #   rm(list = listtmp <- paste0("event_", dfsamplingsprotocols$samplingsprotocols[i]) )
+  #   rm(list = listtmp <- paste0("mof_", dfsamplingsprotocols$samplingsprotocols[i]) )
+  #   rm(list = listtmp <- paste0("occurr_", dfsamplingsprotocols$samplingsprotocols[i]) )
+  # }
+
+  # remove supporting data frames
+  rm(eventswithoccurr)
+  rm(eventswithoutoccurr)
+  rm(extendedmeasurementorfact2)
+  rm(occurrence2)
+  rm(dfoccurrsamplingsprotocols)
+  rm(dfeventsamplingsprotocols)
+  rm(index)
+
+  # remove supporting vectors
+  rm(i)
+  #rm(listtmp)
+  rm(temp)
+
+}
 
