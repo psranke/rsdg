9ba9ab9a2ccf1160d06d6296d373370245052f3e
psranke <35600909+psranke@users.noreply.github.com>
2022-09-04 20:22:57 +0200

    Update rsdg.R
    general fix

--- R/rsdg.R
+++ R/rsdg.R
@@ -1,9 +1,11 @@
 rsdg <- function(){
-
-  filename <- file.choose()
   
   ## Clean Memory
   rm(list=ls())
+
+  .GlobalEnv$filename <- file.choose()
+
+
   
   # load packages
   library(utils)
@@ -14,50 +16,51 @@ rsdg <- function(){
   library(tools)
   
   tmpfolder <- tempfile()
-
+  workfolder <- getwd()
+  
   ## extract just the child
   unzip(file.path(filename), exdir=tmpfolder)
   ff <- file.path(filename)
   index <- unzip(ff,list=TRUE)
   ## Read the file
   temp = list.files(path = tmpfolder, pattern="*.txt")
-
-
+  
+  
   for(i in 1:length(temp)){
-    assign(paste0("raw_", sub("\\..*", "", temp[i]) ), read.csv(paste0(tmpd,'/',file_path_sans_ext(filename), '/',temp[i]), skip = 0, header=T, sep="\t", stringsAsFactors=F), envir = .GlobalEnv)
+    assign(paste0("raw_", sub("\\..*", "", temp[i]) ), read.csv(paste0(tmpfolder, '/',temp[i]), skip = 0, header=T, sep="\t", stringsAsFactors=F), envir = .GlobalEnv)
   }
-
+  
   # first check sampling protocols for special characters
   raw_event$samplingProtocol <- gsub("[[:punct:]]", "_", raw_event$samplingProtocol)
-
-
+  
+  
   # find events with occurrences
   eventswithoccurr <- subset(raw_event, (id %in% raw_occurrence$eventID))
-
+  
   # find events without occurrences
   eventswithoutoccurr <- subset(raw_event, !(id %in% raw_occurrence$eventID))
-
-
-
+  
+  
+  
   # join in sample protocol information to event measurements or facts table
   extendedmeasurementorfact2 <-  merge(x = raw_extendedmeasurementorfact, y = raw_event[,c("id","samplingProtocol")], by.x = "id", by.y= "id")
-
+  
   # join in sample protocol information to occurrence table
   occurrence2 <-  merge(x = raw_occurrence, y = raw_event[,c("id","samplingProtocol")], by.x = "id", by.y= "id")
-
-
-
+  
+  
+  
   # then check sampling protocols, basis for iteration
   raw_event$samplingProtocol <- as.factor(as.character(raw_event$samplingProtocol))
   dfsamplingsprotocols <- data.frame(samplingsprotocols = levels(raw_event$samplingProtocol), stringsAsFactors=T)
-
-
-
-
-
+  
+  
+  
+  
+  
   # OUTPUT TABLES
-
-
+  
+  
   # dcast for mof on occurrence
   # then check sampling protocols, basis for iteration
   eventswithoccurr$samplingProtocol <- as.factor(as.character(eventswithoccurr$samplingProtocol))
@@ -68,8 +71,8 @@ rsdg <- function(){
     # variable description
     assign(paste0("df_", dfoccurrsamplingsprotocols$samplingsprotocols[i],"_desc"), data.frame(subset(extendedmeasurementorfact2, samplingProtocol == dfoccurrsamplingsprotocols$samplingsprotocols[i]) %>% select(measurementType,measurementAccuracy,measurementUnit,measurementMethod) %>% unique()), envir = .GlobalEnv )
   }
-
-
+  
+  
   # dcast for mof on event
   # then check sampling protocols, basis for iteration
   eventswithoutoccurr$samplingProtocol <- as.factor(as.character(eventswithoutoccurr$samplingProtocol))
@@ -80,9 +83,7 @@ rsdg <- function(){
     # variable description
     assign(paste0("df_", dfeventsamplingsprotocols$samplingsprotocols[i],"_desc"), data.frame(subset(extendedmeasurementorfact2, samplingProtocol == dfeventsamplingsprotocols$samplingsprotocols[i]) %>% select(measurementType,measurementAccuracy,measurementUnit,measurementMethod) %>% unique()), envir = .GlobalEnv )
   }
-
-
-
+    
   # remove supporting data frames
   rm(eventswithoccurr)
   rm(eventswithoutoccurr)
@@ -91,10 +92,9 @@ rsdg <- function(){
   rm(dfoccurrsamplingsprotocols)
   rm(dfeventsamplingsprotocols)
   rm(index)
-
+  
   # remove supporting vectors
   rm(i)
-  #rm(listtmp)
+  rm(ff)
   rm(temp)
-
 }
